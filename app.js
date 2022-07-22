78e923c531c004ef4a5847a787f963296f0c16d5
Jeremy Christian <jeremy.l.christian@gmail.com>
2022-07-22 11:39:22 -0700

    Add active/inactive values to HTML to later be used for different functions. Change button colors upon clicking as well as change values.

--- app.js
+++ app.js
@@ -6,6 +6,51 @@ let drawButton = document.getElementById('draw');
 let eraseButton = document.getElementById('erase');
 let rainbowButton = document.getElementById('rainbow');
 
+eraseButton.addEventListener('click', () => {
+    drawButton.value = "inactive";
+    drawButton.classList.add('inactive');
+    drawButton.classList.remove('active');
+    eraseButton.value = "active";
+    eraseButton.classList.add('active');
+    eraseButton.classList.remove('inactive');
+    rainbowButton.value = "inactive";
+    rainbowButton.classList.add('inactive');
+    rainbowButton.classList.remove('active');
+    console.log(eraseButton);
+    console.log(drawButton);
+    console.log(rainbowButton);
+});
+
+rainbowButton.addEventListener('click', () => {
+    drawButton.value = "inactive";
+    drawButton.classList.add('inactive');
+    drawButton.classList.remove('active');
+    eraseButton.value = "inactive";
+    eraseButton.classList.add('inactive');
+    eraseButton.classList.remove('active');
+    rainbowButton.value = "active";
+    rainbowButton.classList.add('active');
+    rainbowButton.classList.remove('inactive');
+    console.log(eraseButton);
+    console.log(drawButton);
+    console.log(rainbowButton);
+});
+
+drawButton.addEventListener('click', () => {
+    drawButton.value = "active";
+    drawButton.classList.add('active');
+    drawButton.classList.remove('inactive');
+    eraseButton.value = "inactive";
+    eraseButton.classList.add('inactive');
+    eraseButton.classList.remove('active');
+    rainbowButton.value = "inactive";
+    rainbowButton.classList.add('inactive');
+    rainbowButton.classList.remove('active');
+    console.log(eraseButton);
+    console.log(drawButton);
+    console.log(rainbowButton);
+});
+
 // This allows grid to automatically be created once the application is open
 let startBoxes = 256;
 grid.style.gridTemplateColumns = "repeat(16, 1fr)";

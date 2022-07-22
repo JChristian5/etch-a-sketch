7baac35ac15904e6b15ded33ba500b1014827d0b
Jeremy Christian <jeremy.l.christian@gmail.com>
2022-07-22 10:29:02 -0700

    Grid automatically appears once application is open

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const grid = document.querySelector('.container');
 
 let slideText = document.querySelector('.slidetext');
 
+let startBoxes = 256;
+grid.style.gridTemplateColumns = "repeat(16, 1fr)";
+for(let i = 0; i < startBoxes; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    grid.appendChild(cell);
+
+    cell.addEventListener('click', () => {
+        cell.classList.add('black');
+    });
+}
+
 function slideValue(newVal) {
     grid.innerHTML = '';
     let size = document.getElementById('slider').value;
